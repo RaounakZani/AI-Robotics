7192a2d5ef1ddf330f2a89478c1a924f6678bb4f
Raounak Zahi <raounakzani@gmail.com>
2026-09-25 20:26:42 +0100

    Add min and max calculations

--- hello_ai.py
+++ hello_ai.py
@@ -4,3 +4,5 @@ numbers = np.array([1, 2, 3, 4, 5])
 
 print("My AI & Robotics environment is working!")
 print("Mean:", numbers.mean())
+print("Maximum:", numbers.max())
+print("Minimum:", numbers.min())
